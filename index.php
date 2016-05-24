a88f13022d5e4f1c1550a9133e8698a0a792282e
Gökhan Demir <gdemir@bil.omu.edu.tr>
2016-05-24 20:23:05 +0300

    .

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <?php
-include 'lib/db.php';
-include 'lib/model.php';
 
+include 'lib/Database.php';
+include 'lib/ApplicationModel.php';
+include 'lib/ApplicationController.php';
+
+///////////////////////////////////////////////////////////
 $db_name = "test";
-DB::connect("localhost", "root", "159654", $db_name);
-
-$table_names = DB::table_names($db_name);
+Database::connect("localhost", "root", "159654", $db_name);
+$table_names = Database::table_names($db_name);
 
 foreach ($table_names as $table_name) {
 	eval("
-		class $name extends ApplicationModel {
-			protected static \$table_name = $name;
+		class $table_name extends ApplicationModel {
+			protected static \$name = '$table_name';
 		}
 	");
- }
-
-print_r(Users::fields());
+}
+		
+print_r(Users::All());
 print_r(Comments::fields());
+///////////////////////////////////////////////////////////
+
+
+// $sonuc = mysql_query("SHOW COLUMNS FROM Users");
+// if (!$sonuc) {
+//     echo 'Sorguyu çalıştıramadı: ' . mysql_error();
+//     exit;
+// }
+// if (mysql_num_rows($sonuc) > 0) {
+//     while ($satir = mysql_fetch_assoc($sonuc)) {
+//         print_r($satir);
+//     }
+// }
 
 echo "<br/>" . $_SERVER['REQUEST_URI'];
+
 ?>
--- lib/ApplicationController.php
+++ lib/ApplicationController.php
@@ -1,11 +1,5 @@
 <?php
 class ApplicationController {
-  public static before_filter = NULL;
-  public function __call($method, $arguments) {
-      if (method_exists($this, $method)) {
-          call_user_func($before_filter);
-          return call_user_func_array(array($this, $method), $arguments);
-      }
-  }
+
 }
 ?>

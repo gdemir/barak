db040a825c410b6d89d181d1e5d233f36e8c0493
Gökhan Demir <mail@gdemir.me>
2016-05-24 08:21:29 +0300

    Update controller.php

--- lib/controller.php
+++ lib/controller.php
@@ -2,7 +2,7 @@
 class Controller {
   public static before_filter = NULL;
   public function __call($method, $arguments) {
-      if(method_exists($this, $method)) {
+      if (method_exists($this, $method)) {
           call_user_func($before_filter);
           return call_user_func_array(array($this, $method), $arguments);
       }

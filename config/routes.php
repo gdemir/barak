43bce3420a6672b482f4512703b81a918acca54c
Gökhan Demir <mail@gdemir.me>
2016-05-25 09:46:01 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,17 @@
 <?php
 Schedule::Application.routes.draw do
-Routes::draw(array(
+Routes::draw(
   "get" => array("/home" => "home#index"),
-));
+);
+
+class Routes {
+  public static function draw()
+  {
+    $arguments = func_get_args();
+    $arguments_count = func_num_args();
+    for ($i = 0; $i < $arguments_count; $i++) {
+        echo "$i. değiştirge: " . $arguments[$i] . "<br />\n";
+    }
+  }
+}
 ?>

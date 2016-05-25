57e4bbe69677e9b0ed2cfa8e2a510ddc85e8813b
Gökhan Demir <mail@gdemir.me>
2016-05-25 15:13:32 +0300

    Update UsersController.php

--- UsersController.php
+++ UsersController.php
@@ -1,13 +1,13 @@
 <?php
 class UsersController extends ApplicationController {
-			before_filter = "require_login";
-			public require_login()
-			{
-			  echo "login olmadan asla";
-			}
-			public login()
-			{
-			  echo "login olduk";
-			}
-		}
+	$before_filter = "require_login";
+	public require_login()
+	{
+	  echo "login olmadan asla";
+	}
+	public login()
+	{
+	  echo "login olduk";
+	}
+}
 ?>

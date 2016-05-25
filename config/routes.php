90fd866ded94e78b922cfd0930cb0b9bc656a557
Gökhan Demir <mail@gdemir.me>
2016-05-25 09:50:09 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,5 @@
 <?php
-Schedule::Application.routes.draw do
+// Schedule::Application.routes.draw do
 Routes::draw(
   get("/", "home#index"),
 );
@@ -8,7 +8,6 @@ class Routes {
   public static function draw()
   {
     $route_functions = func_get_args();
-    $route_functions_count = func_num_args();
     foreach ($route_functions as $route_function)
       echo "değiştirge: " . $route_function . "<br />\n";
   }

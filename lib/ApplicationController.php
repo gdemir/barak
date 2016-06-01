aceb247f6711905b568b82b3f8c2de84ee1dc269
Gökhan Demir <mail@gdemir.me>
2016-06-01 09:13:30 +0300

    Update ApplicationController.php

--- lib/ApplicationController.php
+++ lib/ApplicationController.php
@@ -8,7 +8,7 @@ class ApplicationController {
   public function before_filter() {
     echo "<br/>Çocuk class için geldik, öncelik için bir şeyler yapacağız<br/>";
     $action = $this->filter;
-    if (method_exists($this, 'login')) {
+    if (method_exists($this, $action)) {
       $this->$action();
     }
   }

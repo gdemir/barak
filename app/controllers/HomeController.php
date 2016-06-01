1c8297e64bc85617becba780106d406d5655e771
Gökhan Demir <mail@gdemir.me>
2016-06-01 09:05:39 +0300

    Update HomeController.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,8 +1,11 @@
 <?php
 class HomeController extends ApplicationController {
-
-	public function index () {
-	  echo "Merhaba home#index";
-	}
+  protected $filter = "login";
+  public function index () {
+    echo "Merhaba home#index<br/>";
+  }
+  public function login() {
+    echo "Her işlem öncesi login oluyoruz";
+  }  
 }
 ?>

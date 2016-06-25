ae9e1ebaacc39c73150053cff80c180a6e498133
gdemir <gdemir@bil.omu.edu.tr>
2016-06-25 16:18:02 +0300

    add test page, default files

---
+++ app/controllers/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+class DefaultController extends ApplicationController {
+
+  public function index() {
+  	echo "Default#Controller";
+  }
+
+}
+?>
---
+++ app/controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+class UserController extends ApplicationController {
+
+  public function edit() {
+    echo "UserController#edit";
+    //$this->id = "123456789";
+  }
+
+}
+
+?>

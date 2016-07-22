18d3d7d31c5b1c98fcc56465104859a2fa321503
Gökhan Demir <mail@gdemir.me>
2016-07-22 09:18:44 +0300

    Update ApplicationSql.php

--- lib/ApplicationSql.php
+++ lib/ApplicationSql.php
@@ -1,5 +1,29 @@
 <?php
+// DRAFT #TODO
 class ApplicationSql {
+
+  public static insert($fields, $values) {
+    $GLOBALS["db"]->query(
+        "insert into " . static::$name .
+        " (" . $fields . ") " .
+        "values(" . $values . ")"
+        );
+  }
+
+  public static update($sets, $conditions) {
+    $GLOBALS['db']->query(
+        "update " . static::$name .
+        " set " . $sets .
+        " where " . $conditions
+        );
+  }
+  
+  public static delete($conditions) {
+    $GLOBALS['db']->query(
+        "delete from " . static::$name .
+        " where " . $conditions
+        );
+  }
 
 }
 

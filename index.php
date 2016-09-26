70b6af4e14e865ee2dda3c0bc052ec4e5430f916
Gökhan Demir <mail@gdemir.me>
2016-09-26 09:42:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 // lib/*.php files load
 // app/controllers/*.php  files load
+// app/models/*.php  files load
 
 $directories = [
-            'lib/',
-            'app/controllers/'
+            'lib/',              // system class files
+            'app/controllers/',  // controller class files
+            'app/models/'        // model class files
 ];
 foreach ($directories as $directory) {
     foreach(glob($directory . "*.php") as $class) {
@@ -31,7 +33,7 @@ $GLOBALS['db'] = new BARAK("mysql:host={$database['host']};dbname={$database['na
 
 foreach ($GLOBALS['db']->table_names() as $table_name) {
   eval("
-    class $table_name extends ApplicationModel {
+    class $table_name extends '$table_name'Model {
       protected static \$name = '$table_name';
       public function __construct(\$primary_key = false) {
         parent::__construct(\$primary_key);

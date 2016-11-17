35f40b0d9f501ec59f265a102b98a44e656f6182
gdemir <gdemir@bil.omu.edu.tr>
2016-11-17 13:40:40 +0300

    .

---
+++ app/controllers/LangController.php
@@ -0,0 +1,10 @@
+<?php
+class LangController extends ApplicationController {
+
+  // public function en() {
+  //   $GLOBALS["i18n"]->locale = "en";
+  //   $this->redirect_to("/admin/login");
+  // }
+
+}
+?>
